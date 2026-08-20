d61ef03d8e704ff42905786896bde23cbc2a36e3
Niobium <H1R0SH1M4@proton.me>
2026-08-20 07:51:44 +0300

    feat: project structure, dependencies, environment setup

--- config.py
+++ config.py
@@ -1,18 +1,23 @@
-from typing import Any, Dict
+from typing import Any
 
-CONFIG: Dict[str, Any] = {
+CONFIG: dict[str, Any] = {
     "SEED": 42,
     "MODEL_NAME": "facebook/esm2_t33_650M_UR50D",
-    "BATCH_SIZE": 1,
-    "TARGET_PDB": "8OTA",
-    "ACTIVE_SITE_RESIDUES": [165, 210, 242],
-    "ACTIVE_SITE_RADIUS": 9.0,
-    "PDB_DIR": "./pdbs",
+    "TARGET_PDB": "5XJH",
+    "PDB_DIR": "./data/raw",
+    "BENCHMARK_DIR": "./data/benchmarks",
     "RESULTS_DIR": "./results",
     "PLOT_DIR": "./results/plots",
-    "MUTATIONS_FIX": {}, #{"A": ["ALA-165-SER"]} todo: add docstring,
-    "FIX_KEEP_WATER": False,
+    "TABLES_DIR": "./results/tables",
+    "CATALYTIC_TRIAD_PDB": [160, 206, 237],
+    "OXYANION_HOLE_PDB": [87, 161],
     "PROTONATION_PH": 7.4,
     "PROTONATION_FF": "AMBER",
-    "FF_FILES_OPENMM": ['amber14-all.xml', 'implicit/obc2.xml']
+    "FF_FILES_OPENMM": ["amber14-all.xml", "implicit/obc2.xml"],
+    "N_CLUSTERS": 5,
+    "TEMPERATURE": 0.1,
+    "NUM_BATCHES": 1,
+    "BATCH_SIZE": 16,
+    "MUTATIONS_FIX": {},
+    "FIX_KEEP_WATER": False,
 }
